0c64901240ca4e5dd9e02a0d8afb3a602bee8ac9
admin <admin@pfb04.cshl.edu>
2017-10-17 11:53:08 -0400

    first python script that works

---
+++ python1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# create variable for my answers
+a = "My name is Ying Sun"
+b = "My favorite color is purple"
+c = "My favorite activity is to eat"
+d= "My favorite animal is a bird"
+
+
+# print results in two different ways
+print (a +'\n' + b + '\n' + c + '\n' + d +'\n')
+#print (a)
+#print (b)
+#print (c)
+#print (d)
+
+# don't forget to make your script executible so it runs perfectly
+
---
+++ python2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+c = sys.argv[3]
+d = sys.argv[4]
+
+print ("My name is " + a)
+print ("My favorite color is " + b)
+print ("My favorite activity is to " +  c)
+print ("My favorite animal is " + d)
